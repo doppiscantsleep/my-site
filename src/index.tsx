48da5dd8c23efa117c53440c025a6439663a96ce
Alan DeBoer <alan@air-m2.local>
2025-03-30 15:49:11 -0400

    markdown stuff

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,20 @@ const resumeText = marked(`## Senior Network Engineer - Hertz
   - Lead engineer that writes project/migration MOPs
   - Responsible for security inspection VPCs utilizing Check Point firewalls
   - Responsible for BloxOne Threat Defense, SaaS service for external DNS lookups
-  - Responsible for all Check Point firewalls running in AWS`);
+  - Responsible for all Check Point firewalls running in AWS
+
+## DevOps Security Engineer – TOMTOM
+### (June 2020 – September 2023)
+  - Responsible for 30+ Palo Alto firewalls at our branch offices and our Datacenters 
+  - Responsible for Panorama, the management system for our Palo Alto firewalls. 
+  - Models of Palo Altos I am familiar with: PA-5260, 5060, 3260, 3050, 820, 460 and 220s 
+  - Responsible for our global client VPN solution, Ivanti/Pulse Secure. 
+  - Responsible for VPN connectivity to our partners via IPsec tunnels. 
+  - Experience with AWS: VPCs, IGWs, NGWs, Security Groups, Route tables, Direct Connects and other miscellaneous networking services. 
+  - Responsible for managing groups of Linux servers with Ansible, mostly using Ansible for vulnerability patching and running ad hoc commands 
+  - Experience with deploying public cloud infrastructure with Terraform, mostly used to replicate traditional on-premise infrastructure in Azure or AWS as a POC. 
+  - Team lead on the migration of our network monitoring solution UMI/Nagios to LogicMonitor, a new implementation. 
+  - Team lead on our ISO 27K audit with regards to Network Services`);
 
 const app = new Hono()
 
